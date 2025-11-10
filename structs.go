c4a0f10f2b6d5f2d5b1ba5d0b3314dc0d8f28dcd
N1RN4M4 <iddeen43442@gmail.com>
2025-11-10 10:10:01 +0800

    Complete basic function for the code

--- structs.go
+++ structs.go
@@ -1,7 +1,21 @@
 package main
 
+import "fmt"
+
 func main() {
-	// firstName
+	// firstName //will take from the new func
+	firstName := getUserInfo("First Name: ")
 	// lastName
+	lastName := getUserInfo("Last Name: ")
 	// birthDate
+	birthDate := getUserInfo("Birth Date: ")
+
+	fmt.Println(firstName, lastName, birthDate)
+}
+
+func getUserInfo(userInput string) string {
+	fmt.Print(userInput)
+	var value string
+	fmt.Scan(&value)
+	return value
 }
